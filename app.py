74da16802028e8c773bc8f0229894daccf95f16c
Santhosh Shanmugam <santhoshs192005@gmail.com>
2025-01-17 06:36:32 +0530

    recommended system

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import requests
+import pickle
+
+with open('data.pkl', 'rb') as file:
+    movies, cosine_sim = pickle.load(file)
+
+def get_recommendations(title, cosine_sim=cosine_sim):
+    idx = movies[movies['title'] == title].index[0]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]  # exclude itself and itself's similarity score
+    movie_indices = [i[0] for i in sim_scores]
+    return movies.iloc[movie_indices]
+
+def fetch_poster(movie_id):
+    api_key = 'bbec780a2b184df99f28bbc1aa520691'
+    url = f'http://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    poster_path = data['poster_path']
+    full_path = f"https://image.tmdb.org/t/p/w500{poster_path}"
+    return full_path
+
+st.title("Movie Recommended System")
+
+selected_movie = st.selectbox("Select a movie", movies['title'].values)
+
+if st.button('Recommend'):
+    recommendations = get_recommendations(selected_movie)
+    st.write("Top 10 Movies to Watch")
+
+    for i in range(0, 10, 5):
+        cols = st.columns(5)
+        for col, j in zip(cols, range(i, i + 5)):
+            if j < len(recommendations):
+                movie_title = recommendations.iloc[j]['title']
+                movie_id = recommendations.iloc[j]['movie_id']  # Ensure movie_id exists in the DataFrame
+                poster_url = fetch_poster(movie_id)
+                with col: 
+                    st.image(poster_url, width=130)
+                    st.write(movie_title)
